5c254c8b81c6f923290d530797c6a997efba8866
Christianazu007 <116691515+Christianazu007@users.noreply.github.com>
2022-10-25 21:17:37 -0300

    Create CatApi.js

---
+++ JS/CatApi.js
@@ -0,0 +1,51 @@
+const url = `https://api.thecatapi.com/v1/breeds`;
+   const api_key = "live_j9UhjlyVBX2EVZD5nMY0r7aW22CLD9HNvnm8m3g8a2WtLmgNPFaazo4yVIJrJYUJ"
+  
+let storedBreeds = []
+   var num;
+  
+/**
+ * Funcion para cambiar la imagen
+ */
+ function rotarImagenes()
+ {
+
+  fetch(url,{headers: {
+    'x-api-key': api_key
+  }})
+
+.then((response) => {
+ return response.json();
+})
+.then((data) => {
+
+data = data.filter(img=> img.image?.url!=null)
+ 
+storedBreeds = data;
+
+   // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
+   var index=Math.floor((Math.random()*storedBreeds.length));
+ 
+   // cambiamos la imagen
+   document.getElementById("img").src=storedBreeds[index].image.url;
+  })
+  .catch(function(error) {
+     console.log(error);
+  });
+}
+
+  /**
+  * Función que se ejecuta una vez cargada la página
+  */
+ onload=function()
+ {
+   // Cargamos una imagen aleatoria
+   rotarImagenes();
+ 
+   // Indicamos que cada 5 segundos cambie la imagen
+   setInterval(rotarImagenes,5000);
+
+   rotarImagenes1();
+   setInterval(rotarImagenes1,5000);
+
+ }
